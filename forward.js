58cb91b9503223fba2fcfc09924416f24c1ade4e
whym <whym@whym.org>
2011-09-12 14:59:15 +0900

    * forwarding app to ja.wikipedia.org

---
+++ forward.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+app.get(/.*/, function(req, res) {
+  var destination = 'http://ja.wikipedia.org/wiki' + req.url;
+  res.writeHead(301, {'Content-Type': 'text/html; charset=UTF-8',
+					  'Location': destination });
+  res.end('<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN"><html><head><title>301 Moved Permanently</title></head><body><h1>Moved Permanently</h1><p>The document has moved <a href="'+ destination +'">here</a>.</p></body></html>');
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
