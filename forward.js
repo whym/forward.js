2bc76248836917a2cb4f593fc0de7c74ab010676
Yusuke Matsubara <whym@whym.org>
2016-03-24 20:15:37 +0900

    Wrap app

--- forward.js
+++ forward.js
@@ -1,16 +1,28 @@
 var express = require('express');
-var app = express.createServer(express.logger());
-app.forward_target = process.env.FORWARD_TARGET || 'http://en.wikipedia.org/wiki';
-app.get(/.*/, function(req, res) {
-	var destination = process.env.NO_ARG ? app.forward_target : app.forward_target + req.url;
-	res.writeHead(301, {'Content-Type': 'text/html; charset=UTF-8',
-						'Location': destination });
-	res.end('<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN"><html><head><title>301 Moved Permanently</title></head><body><h1>Moved Permanently</h1><p>The document has moved <a href="'+ destination +'">here</a>.</p></body></html>');
-});
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-	console.log("Listening on " + port + " forwarding to " + app.forward_target);
-});
+var forward = function(target, no_arg, port) {
+	port = port || 3000;
 
-module.exports = app;
+	var app = express.createServer(express.logger());
+	app.get(/.*/, function(req, res) {
+		var destination = no_arg ? target : target + req.url;
+		res.writeHead(301, {'Content-Type': 'text/html; charset=UTF-8',
+							'Location': destination });
+		res.end('<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN"><html><head><title>301 Moved Permanently</title></head><body><h1>Moved Permanently</h1><p>The document has moved <a href="'+ destination +'">here</a>.</p></body></html>');
+	});
+
+	app.listen(port, function() {
+		console.log('Listening on '+ port + ' forwarding to ' + target);
+	});
+	return {app: app};
+};
+
+if ( require.main === module ) {
+	forward(
+		process.env.FORWARD_TARGET || 'http://en.wikipedia.org/wiki',
+		process.env.NO_ARG ? true: false,
+		process.env.PORT
+	);
+}
+
+module.exports = forward;
--- test/forward_test.js
+++ test/forward_test.js
@@ -1,19 +1,18 @@
 var request = require('supertest');
-var app = require('../forward');
+var forward = require('../forward');
 
 describe('/', function() {
-    it('redirected (301)', function(done) {
+	var app = forward('http://example.com').app;
+    it('redirects (301)', function(done) {
         request(app)
             .get('/')
             .expect('Content-Type', 'text/html; charset=UTF-8')
-            .expect(301)
-			.end(done);
+            .expect(301, done);
     });
-    it('"example.com/poke" specified', function(done) {
-		app.forward_target = 'http://example.com';
+    it('redirects to http://example.com/poke given /poke', function(done) {
         request(app)
             .get('/poke')
             .expect('<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN"><html><head><title>301 Moved Permanently</title></head><body><h1>Moved Permanently</h1><p>The document has moved <a href="http://example.com/poke">here</a>.</p></body></html>')
-			.end(done);
+			.expect(301, done);
     });
 });
